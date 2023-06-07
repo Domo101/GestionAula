16a0b35d3687cb1b942c436e41946c9fcc8f5ef6
David Coello Pulido <davidcoello@eprosima.com>
2023-06-07 13:09:26 +0200

    E3: Anadir alumno
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

--- src/gestion_aula.py
+++ src/gestion_aula.py
@@ -1,12 +1,26 @@
 def mostrar_clase(clase):
+    """mostrar la clase por pantalla"""
     num_alumno = 0
     for alumno in clase:
         num_alumno = num_alumno + 1
         # num_alumno: nombre_alumno
+        # print(str(num_alumno) + ": " + alumno)
         print(f"{num_alumno}: {alumno.capitalize()}")
+
+def nuevo_alumno():
+    """pedir nuevo alumno al ususario"""
+    alumno = input("inserte nuevo alumno: ")
+    print(f"Saludos {alumno}!")
+    return alumno
 
 # definir
 clase = ["acenha", "jorge", "alex"]
+
+# nuevo alumno
+n_a = nuevo_alumno()
+
+# añadir
+clase.append(n_a)
 
 # ordenar
 clase.sort()
