36e3f91e5502abd5398f5adc143c2b7688b334ff
eliote62 <coellodelgado@hotmail.com>
2023-06-12 00:05:59 +0200

    se aplica autopep8
    escribir en terminal: autopep8 --in-place -a -a gestion_aula_v1.py

--- src/gestion_aula_v1.py
+++ src/gestion_aula_v1.py
@@ -1,6 +1,8 @@
 import csv
 import json
 import random
+
+
 def guardar_clase(clase):
     """guarda la clase en un fichero"""
     # escribir en un archivo
@@ -14,6 +16,8 @@ def guardar_clase(clase):
     archivo.write(clase_en_json)
     # cerramos archivo
     archivo.close()
+
+
 def cargar_clase():
     """cargar la clase desde un fichero"""
     try:
@@ -29,30 +33,41 @@ def cargar_clase():
         clase = list()
     # retornamos el array de la clase
     return clase
+
+
 def nuevo_alumno():
     """pedir nuevo alumno al ususario, se parte con una puntuacion de 5"""
     alumno = dict(nombre=input("inserte nuevo alumno: "), puntuacion=5)
     print(f"Saludos {alumno['nombre']}!")
     return alumno
+
+
 def esta_en_clase(alumno_entrada, clase):
     """comprobar si alumno esta en la clase"""
     for alumno in clase:
         if alumno["nombre"] == alumno_entrada["nombre"]:
             return True
     return False
+
+
 def seleccionar_alumno_random(clase):
     indice_random = random.randint(0, len(clase) - 1)
     return clase[indice_random]
+
+
 def print_alumno(alumno):
-    return f"{alumno['nombre'].capitalize()} ({alumno['puntuacion']})"  
-def mostrar_clase(clase):                      
+    return f"{alumno['nombre'].capitalize()} ({alumno['puntuacion']})"
+
+
+def mostrar_clase(clase):
     """mostrar la clase por pantalla"""
     num_alumno = 0
     for alumno in clase:
         num_alumno = num_alumno + 1
         # num_alumno: nombre_alumno
-        print(f"{num_alumno}: {print_alumno(alumno)}")     
-        
+        print(f"{num_alumno}: {print_alumno(alumno)}")
+
+
 def volcar_datos(clase):
     with open('clase.csv', 'w', newline='') as archivo_csv:
         nombres_columnas = ['nombre', 'puntuacion']
@@ -62,15 +77,19 @@ def volcar_datos(clase):
             writer.writerow(alumno)
     print("cambios volcados en el archivo clase.csv")
 
-def seleccionar_alumno(clase):              
-    mostrar_clase(clase)                   
-    num_alumno = int(input("Alumno: "))     
-    return clase[num_alumno - 1]            
+
+def seleccionar_alumno(clase):
+    mostrar_clase(clase)
+    num_alumno = int(input("Alumno: "))
+    return clase[num_alumno - 1]
+
+
 def positivo(alumno, clase):
     for al in clase:
         if alumno["nombre"] == al["nombre"]:
             al["puntuacion"] = al["puntuacion"] + 1
             print(f"Positivo: {print_alumno(al)}")
+
 
 def negativo(alumno, clase):
     for al in clase:
@@ -78,42 +97,47 @@ def negativo(alumno, clase):
             al["puntuacion"] = al["puntuacion"] - 1
             print(f"puntuacion: {print_alumno(al)}")
 
-def submenu_seleccionar(clase):                            
+
+def submenu_seleccionar(clase):
     """submenu de acciones sobre alumno"""
-    alumno = seleccionar_alumno(clase)                     
+    alumno = seleccionar_alumno(clase)
     print(f"Seleccionado: {print_alumno(alumno)}")
     decision = ""
     while decision != "r":
-        decision = input("Seleccionar \n\t(+/positivo) \n\t(-/negativo) \n\t(r/return)\n-> ")
+        decision = input(
+            "Seleccionar \n\t(+/positivo) \n\t(-/negativo) \n\t(r/return)\n-> ")
         if decision == "+":
             positivo(alumno, clase)
         elif decision == "-":
             negativo(alumno, clase)
 
+
 def expulsion_alumno(clase):
     """expulsa un alumno cuando su puntuacion llega a 0"""
-    i = 0    
+    i = 0
     for al in clase:
-        if al["puntuacion"]<1:
-            print(f"Bye bye " + str(clase[i]['nombre'].capitalize())+ "!, I'm sorry...")
+        if al["puntuacion"] < 1:
+            print(f"Bye bye " +
+                  str(clase[i]['nombre'].capitalize()) +
+                  "!, I'm sorry...")
             del clase[i]
         else:
-            i = i+1    
-    
-
+            i = i + 1
 
 
 def bucle_decisiones(clase):
     """bucle de decisiones"""
     decision = ""
     while decision != "q":
-        decision = input("Accion \n\t(q/salir) \n\t(r/random) \n\t(m/mostrar) \n\t(v/volcar) \n\t(e/expulsar) \n\t(g/guardar) \n\t(s/seleccionar) \n\t(n/nuevo)\n-> ")
+        decision = input(
+            "Accion \n\t(q/salir) \n\t(r/random) \n\t(m/mostrar) \n\t(v/volcar) \n\t(e/expulsar) \n\t(g/guardar) \n\t(s/seleccionar) \n\t(n/nuevo)\n-> ")
         if decision == "m":
             # mostrar clase
             mostrar_clase(clase)
         elif decision == "r":
             # seleccionar alumno random
-            print(f"Seleccionado: {print_alumno(seleccionar_alumno_random(clase))}")
+            print(
+                f"Seleccionado: {print_alumno(seleccionar_alumno_random(clase))}")
         elif decision == "n":
             # nuevo alumno
             n_alumno = nuevo_alumno()
@@ -128,12 +152,13 @@ def bucle_decisiones(clase):
         elif decision == "v":
             volcar_datos(clase)
         elif decision == "s":
-            submenu_seleccionar(clase)                  
+            submenu_seleccionar(clase)
         elif decision == "e":
             expulsion_alumno(clase)
         elif decision == "g":
             guardar_clase(clase)
             print("cambios guardados en el archivo bd.json")
+
 
 # definir
 clase = cargar_clase()
