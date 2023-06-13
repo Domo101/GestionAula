c76bd713753a44a366e48474b182e1cef2317687
lx-vzqz <avcarral@outlook.com>
2023-06-13 21:02:19 +0200

    Arreglar PEP8

--- src/gestion_aula.py
+++ src/gestion_aula.py
@@ -99,11 +99,13 @@ def positivo(alumno, clase):
             al["positivos"] = al["positivos"] + 1
             print(f"Positivo: {print_alumno(al)}")
 
+
 def negativo(alumno, clase):
     for al in clase:
         if alumno["nombre"] == al["nombre"]:
             al["positivos"] = al["positivos"] - 1
             print(f"Positivo: {print_alumno(al)}")
+
 
 def submenu_seleccionar(clase):
     """submenu de acciones sobre alumno"""
@@ -111,7 +113,8 @@ def submenu_seleccionar(clase):
     print(f"Seleccionado: {print_alumno(alumno)}")
     decision = ""
     while decision != "r":
-        decision = input("Seleccionar \n\t(+/positivo) \n\t(-/negativo) \n\t(r/return)\n-> ")
+        decision = input(
+            "Seleccionar \n\t(+/positivo) \n\t(-/negativo) \n\t(r/return)\n-> ")
         if decision == "+":
             positivo(alumno, clase)
         elif decision == "-":
